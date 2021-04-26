9e54302dc4dbef8a580aa946b5f174de40824a38
root <root@localhost.localdomain>
2021-04-26 13:50:27 +0600

    persist got added

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,19 @@
 import { combineReducers } from "redux";
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userReducer from './user/user.reducer';
+import CardReducer from './cart/cart.reducer';
 
-import userReducer from './user/user.reducer'
-import CardReducer from './cart/cart.reducer'
+const persistConfig = {
+    key: 'root',
+    storage,
+    whiteList: ['cart']
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
     user: userReducer,
     cart: CardReducer
 
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import {applyMiddleware, createStore} from "redux";
 import logger from  'redux-logger';
 import rootReducer from './root-reducer';
+import {persistStore} from 'redux-persist'
 
 const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store)
 
-export default store;
+export default {store, persistor};
