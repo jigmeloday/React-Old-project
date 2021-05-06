adf9f10c48be89e3d2b123025fd171d431aadf67
root <root@localhost.localdomain>
2021-05-06 17:58:10 +0600

    update in shop data

--- src/page/collection/collection.component.jsx
+++ src/page/collection/collection.component.jsx
@@ -5,9 +5,15 @@ import { selectCollection } from "../../redux/shop/shop.selector";
 import CollectionItem from "../../component/collection-item/collection-item.component";
 
 const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <div className="title">Hello</div>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item}></CollectionItem>
+        ))}
+      </div>
     </div>
   );
 };
