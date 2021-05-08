fea2dd0a20c8e373e67b5a4d81a641fe6b347e04
root <root@localhost.localdomain>
2021-05-08 23:35:26 +0600

    using promises

--- src/page/shopPage/shop.component.jsx
+++ src/page/shopPage/shop.component.jsx
@@ -21,7 +21,7 @@ class ShopPage extends React.Component {
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
-    collectionRef.onSnapshot(async (snapshot) => {
+    collectionRef.get().then((snapshot) => {
       const collectionMap = convertCollectionsSnapShotToMap(snapshot);
       updateCollections(collectionMap);
       this.setState({ loading: false });
